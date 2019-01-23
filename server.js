7129ba71f30be818da7bd7de98b7ced01a8289f7
JosephIrvin <josephirvin13@gmail.com>
2019-01-23 11:46:07 -0500

    git init

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('morgan')
+// const routes = require('./routes/index')
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+// app.use('/', routes)
+
+const db = mongoose.connection
+db.on('error', err => {
+  console.log(err)
+})
+
+db.on('open', () => {
+  console.log('Connected to MongoDB')
+})
+
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT)
+})
